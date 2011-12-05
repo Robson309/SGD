7b9e36ad0c1c8dfe0919be6fa728c050190b3281
s8133 <kasiek16@gmail.com>
2011-12-05 19:29:11 +0100

    update prisoner1.cpp 3#

--- DylematWieznia/PrisonerA/prisoner1.cpp
+++ DylematWieznia/PrisonerA/prisoner1.cpp
@@ -11,13 +11,11 @@ int main(int argc, char** argv[])
 	string prisoner1_choice = "prisoner1_choice.txt";
 	ofstream prisoner1;
 	prisoner1.open(prisoner1_choice.c_str(), ios::app);
-	
 	srand( time(NULL) );
 	
-		if ((rand() % 100)%2) {
-			prisoner1 << "F";
-		}
-		else prisoner1 << "U";
+		if ((rand() % 100)%2) prisoner1 << "U";
+		else prisoner1 << "F";
+
 	prisoner1.close();
 
 }
